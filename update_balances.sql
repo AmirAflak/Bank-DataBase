b9f25c14790a73f117065acdfdaca4b18bbf2434
Amirreza Aflakparast <84932095+AmirAflak@users.noreply.github.com>
2023-05-12 19:52:49 +0430

    added interest rate calculation

--- update_balances.sql
+++ update_balances.sql
@@ -22,8 +22,9 @@ BEGIN
         DECLARE @Current_Balance float;
 		DECLARE @snapshotID int; 
 		DECLARE @table_name varchar(20);
+		DECLARE @interest_rate float;
 
-		IF @type in ('deposit', 'interest')
+		IF @type = 'deposit'
 		begin 
         SELECT 
             @Current_Balance = amount 
@@ -33,6 +34,30 @@ BEGIN
             accountNumber = @to_destination    
         UPDATE latest_balances 
         SET amount = @Current_Balance + @amount 
+        WHERE accountNumber = @to_destination;
+		INSERT INTO snapshot_log
+		VALUES (DEFAULT);
+		select @snapshotID = MAX(snapshot_id) from snapshot_log;
+		SET @table_Name = CONCAT('snapshot_', @snapshotID);
+		EXEC('CREATE TABLE ' + @table_Name + 
+          'AS SELECT * FROM latest_balances');
+		END
+
+		IF @type = 'interest'
+		begin 
+        SELECT 
+            @Current_Balance = amount 
+        FROM 
+            latest_balances 
+        WHERE 
+            accountNumber = @to_destination;
+		SELECT @interest_rate = interest_rate
+		FROM
+			account 
+		WHERE 
+			accountNumber = @to_destination;
+        UPDATE latest_balances 
+        SET amount = (@Current_Balance) + (@amount * (1+(@interest_rate/100)))
         WHERE accountNumber = @to_destination;
 		INSERT INTO snapshot_log
 		VALUES (DEFAULT);
