3c74bab0cb439983b4acf5f14d75e9839e8b54d4
Amirreza Aflakparast <84932095+AmirAflak@users.noreply.github.com>
2023-05-12 19:00:08 +0430

    new update balances procedure (altering latest_balances table)

--- update_balances.sql
+++ update_balances.sql
@@ -1,37 +1,65 @@
+
 CREATE PROCEDURE Update_Latest_Balances
 AS
 BEGIN
-    DECLARE @Customer_Account_Number int 
-    DECLARE @Transaction_Amount money
+    DECLARE @from_source char(16); 
+    DECLARE @to_destination char(16);
+	DECLARE @type varchar(10);
+	DECLARE @amount float;
 
     DECLARE cur_transactions CURSOR 
     FOR
-        SELECT * 
+        SELECT from_source, to_destination, type, amount 
         FROM transactions
 
     OPEN cur_transactions
 
     FETCH NEXT FROM cur_transactions
-    INTO @Customer_Account_Number, @Transaction_Amount
+    INTO @from_source, @to_destination, @type, @amount
 
     WHILE @@FETCH_STATUS = 0 
     BEGIN
         DECLARE @Current_Balance money
 
+		IF @type in ('deposit', 'interest')
+		begin 
         SELECT 
             @Current_Balance = amount 
         FROM 
             latest_balances 
         WHERE 
-            accountNumber = @Customer_Account_Number
+            accountNumber = @to_destination    
+        UPDATE latest_balances 
+        SET amount = @Current_Balance + @amount 
+        WHERE accountNumber = @to_destination
+		END
 
-        IF @Current_Balance IS NULL
-        BEGIN
-            INSERT INTO latest_balances (accountNumber, amount) 
-            VALUES (@Customer_Account_Number, @Transaction_Amount)
-        END 
-        ELSE 
-        BEGIN
-            UPDATE latest_balances 
-            SET amount = @Current_Balance + @Transaction_Amount 
-            WHERE accountNumber
+		IF @type = 'withdraw'
+		begin 
+        SELECT 
+            @Current_Balance = amount 
+        FROM 
+            latest_balances 
+        WHERE 
+            accountNumber = @from_source
+		UPDATE latest_balances 
+        SET amount = @Current_Balance + @amount 
+        WHERE accountNumber = @from_source
+		END 
+
+		IF @type='transfer'
+		BEGIN
+		UPDATE latest_balances 
+        SET amount = @Current_Balance - @amount 
+        WHERE accountNumber = @from_source
+		UPDATE latest_balances 
+        SET amount = @Current_Balance + @amount 
+        WHERE accountNumber = @to_destination
+		END
+
+        FETCH NEXT FROM cur_transactions
+        INTO @from_source, @to_destination, @type, @amount
+    END
+    CLOSE cur_transactions
+    DEALLOCATE cur_transactions
+END;
